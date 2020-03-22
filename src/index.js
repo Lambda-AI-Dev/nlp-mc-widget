d86c02a4916772d3beddce1bb7f8817a384cbdf3
Daniel Tian <dtian.95@gmail.com>
2020-03-22 15:46:23 +0800

    added dummy ui

--- src/index.js
+++ src/index.js
@@ -1,19 +1,141 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { DatePicker } from "antd";
+import style from "./style";
+import {
+  Paper,
+  Typography,
+  Backdrop,
+  Grid,
+  Button,
+  CircularProgress
+} from "@material-ui/core";
 
-const App = () => {
+const INJECT_DIV_TAG = "lambda-target";
+const API_ID_NAME = "api_id";
+
+const App = ({ api_id }) => {
+  const [showHello, setShowHello] = useState(true);
+  const [showWidget, setShowWidget] = useState(true);
+
+  useEffect(() => {
+    console.log(api_id);
+  }, []);
+
+  useEffect(() => {
+    const onMouseMove = e => {
+      console.log(e);
+    };
+    window.addEventListener("mousemove", onMouseMove);
+
+    return () => {
+      console.log("unmount");
+      window.removeEventListener("mousemove", onMouseMove);
+    };
+  }, []);
+
   return (
     <div>
-      <h1>hello world</h1>
-      This is a React component inside of webflow!! DatePicker should be
-      displayed here:
-      <DatePicker />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      {showWidget && (
+        <Backdrop open={true}>
+          {showHello ? (
+            <Paper style={style.wforeground} elevation={0}>
+              <div style={{ margin: "60px" }}>
+                <Typography variant="h3" style={{ color: "white" }}>
+                  Hey, Help us with a quick question.
+                </Typography>
+
+                <Typography
+                  variant="subtitle2"
+                  style={{
+                    color: "white",
+                    marginTop: "20px",
+                    marginBottom: "20px"
+                  }}
+                >
+                  Please help us with a 5 second question and earn 100 points!
+                  This widget is supported by: Google AI.
+                </Typography>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    setShowHello(false);
+                  }}
+                >
+                  Continue
+                </Button>
+              </div>
+            </Paper>
+          ) : (
+            <Paper style={style.foreground} elevation={0}>
+              <div>
+                <Typography
+                  variant="subtitle2"
+                  style={{ marginLeft: "20px", marginTop: "20px" }}
+                >
+                  Complete Task to Continue
+                </Typography>
+
+                <Paper
+                  style={{ margin: "20px", padding: "20px" }}
+                  elevation={1}
+                >
+                  <center>
+                    <Typography>
+                      Access quote of the day service. Use this to get the quote
+                      of the day in various categories. This is a free API that
+                      is available to public. You must credit They Said So if
+                      you are using the free version
+                    </Typography>
+                  </center>
+                </Paper>
+                <Grid container spacing={3} style={{ padding: "0px 20px" }}>
+                  <Grid item xs={6}>
+                    <Paper style={{ padding: "10px" }}>
+                      <Typography>Hello</Typography>
+                    </Paper>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Paper style={{ padding: "10px" }}>
+                      <Typography>Hello</Typography>
+                    </Paper>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Paper style={{ padding: "10px" }}>
+                      <Typography>Hello</Typography>
+                    </Paper>
+                  </Grid>
+                  <Grid item xs={6}>
+                    <Paper style={{ padding: "10px" }}>
+                      <Typography>Hello</Typography>
+                    </Paper>
+                  </Grid>
+                </Grid>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  style={{ display: "block", float: "right", margin: "20px" }}
+                  onClick={() => {
+                    setShowWidget(false);
+                  }}
+                >
+                  Submit
+                </Button>
+              </div>
+            </Paper>
+          )}
+        </Backdrop>
+      )}
     </div>
   );
 };
 
 ReactDOM.render(
-  React.createElement(App, {}, null),
-  document.getElementById("react-target")
+  React.createElement(App, {
+    api_id: document.getElementById(INJECT_DIV_TAG).getAttribute(API_ID_NAME)
+  }),
+  document.getElementById(INJECT_DIV_TAG)
 );
---
+++ src/style.js
@@ -0,0 +1,35 @@
+const style = {
+  background: {
+    position: "fixed",
+    width: "100%",
+    height: "100%",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    margin: "auto",
+    backgroundColor: "rgba(0, 0, 0, 0.5)"
+  },
+  foreground: {
+    position: "absolute",
+    height: "400px",
+    width: "600px",
+    marginLeft: "-300px",
+    marginTop: "-200px",
+    top: "50%",
+    left: "50%",
+    background: "linear-gradient(to right, #e0eafc, #cfdef3)"
+  },
+  wforeground: {
+    position: "absolute",
+    height: "400px",
+    width: "600px",
+    marginLeft: "-300px",
+    marginTop: "-200px",
+    top: "50%",
+    left: "50%",
+    background: "#3498db"
+  }
+};
+
+export default style;
